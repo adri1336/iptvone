1d22f4e20caa6943c1634d64dd29a74651cdd644
adri1336 <adri1pawn@gmail.com>
2023-04-13 17:20:54 +0200

    update

--- pages/library.jsx
+++ pages/library.jsx
@@ -24,47 +24,46 @@ export default () => {
         focusSelf();
     }, [focusSelf]);
 
-    if(playItem) {
-        const url = playItem.url.replace('/ts', '/m3u8').replace(".ts", ".m3u8");
-        return <div className="page d-flex flex-column">
-            <Player
-                channelName={ playItem.name }
-                url={ url}
-                width='100vw'
-                height='100vh'
-                onClose={ () => setPlayItem(null) }
-            />
-        </div>
-    }
-    else {
-        return (<FocusContext.Provider value={ focusKey }>
-            <div className="page d-flex flex-row">
-                <Sidebar onGroupSelected={ index => setSelectedGroupIndex(index) }/>
-                <div className="mpage">
-                    {
-                        selectedGroupIndex === null ?
-                            <div className="d-flex w-100 align-items-center justify-content-center">
-                                <span className="text-medium fw-bold">{ t('PAGES.LIBRARY.SELECT_GROUP') }</span>
-                            </div> :
-                            <div className="d-flex flex-column">
-                                <span className="title-small fw-bold mb-40">{ (IPTV.getGroups())[selectedGroupIndex] }</span>
-                                <div className="d-flex flex-wrap">
-                                    {
-                                        items.slice(0, 20).map((item, index) => {
-                                            return <Item
-                                                key={ index }
-                                                item={ item }
-                                                onSelected={ () => setPlayItem(item) }
-                                            />
-                                        })
-                                    }
-                                </div>
-                            </div>
-                    }
-                </div>
+    
+    return (<FocusContext.Provider value={ focusKey }>
+        {
+            playItem &&
+            <div style={{ position: 'fixed', top: 0, left: 0 }}>
+                <Player
+                    channelName={ playItem.name }
+                    url={ playItem.url.replace('/ts', '/m3u8').replace(".ts", ".m3u8") }
+                    width='100vw'
+                    height='100vh'
+                    onClose={ () => setPlayItem(null) }
+                />
             </div>
-        </FocusContext.Provider>)
-    }
+        }
+        <div className="page d-flex flex-row" style={{ visibility: playItem ? 'hidden' : 'visible' }}>
+            <Sidebar onGroupSelected={ index => setSelectedGroupIndex(index) }/>
+            <div className="mpage">
+                {
+                    selectedGroupIndex === null ?
+                        <div className="d-flex w-100 align-items-center justify-content-center">
+                            <span className="text-medium fw-bold">{ t('PAGES.LIBRARY.SELECT_GROUP') }</span>
+                        </div> :
+                        <div className="d-flex flex-column">
+                            <span className="title-small fw-bold mb-40">{ (IPTV.getGroups())[selectedGroupIndex] }</span>
+                            <div className="d-flex flex-wrap">
+                                {
+                                    items.map((item, index) => {
+                                        return <Item
+                                            key={ index }
+                                            item={ item }
+                                            onSelected={ () => setPlayItem(item) }
+                                        />
+                                    })
+                                }
+                            </div>
+                        </div>
+                }
+            </div>
+        </div>
+    </FocusContext.Provider>);
 };
 
 export async function getStaticProps({ locale }) {
