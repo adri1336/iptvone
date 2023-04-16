8c88c675e215fd0bcbb369f0a9b127d69f79305c
adri1samp <adri1pawn@gmail.com>
2023-04-16 19:48:49 +0200

    render image on view

--- components/item.jsx
+++ components/item.jsx
@@ -1,10 +1,13 @@
 import styles from "@/styles/item.module.css";
 import IPTV from "@/utils/iptv";
 import { useFocusable } from "@noriginmedia/norigin-spatial-navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 const Item = ({ item, onSelected, focusKey, onFocus }) => {
-    const [ hasImage, setHasImage ] = useState(false);
+    const { ref: viewRef, inView, entry } = useInView({});
+
+    const [ renderImage, setRenderImage ] = useState(null);
     const { ref, focused } = useFocusable({
         onEnterPress: () => onSelected(),
         onFocus: () => {
@@ -17,26 +20,36 @@ const Item = ({ item, onSelected, focusKey, onFocus }) => {
         focusKey: focusKey,
     });
 
-    const raw = item.raw;
-    const image = IPTV.getRawValue(raw, "tvg-logo");
-    if(image) {
-        const img = new Image();
-        img.src = image;
-        img.onload = () => setHasImage(true);
-    }
     
-    if(!hasImage) {
-        return <div ref={ ref } className={ styles.itemContainer + " d-flex flex-column justify-content-center align-items-center" + (focused ? " " + styles.itemfocused : "") } onClick={ () => onSelected() }>
-            <span className="text-medium text-center">{ item.name }</span>
-        </div>
+    useEffect(() => {
+        if(inView && !renderImage) {
+            const raw = item.raw;
+            const image = IPTV.getRawValue(raw, "tvg-logo");
+            if(image) {
+                const img = new Image();
+                img.src = image;
+                img.onload = () => setRenderImage(image);
+            }
+        }
+        else if(!inView && renderImage) setRenderImage(null);
+    }, [inView]);
+    
+    if(!renderImage) {
+        return (<div ref={ viewRef }>
+            <div ref={ ref } className={ styles.itemContainer + " d-flex flex-column justify-content-center align-items-center" + (focused ? " " + styles.itemfocused : "") } onClick={ () => onSelected() }>
+                <span className="text-medium text-center">{ item.name }</span>
+            </div>
+        </div>);
     }
     else {
-        return <div ref={ ref } className={ styles.itemContainer + " d-flex flex-column justify-content-center align-items-center" + (focused ? " " + styles.itemfocused : "") } onClick={ () => onSelected() }>
-            <img src={ image } alt="logo" className={ styles.itemCoverLogo }/>
-            <div className={ styles.itemCoverTitle }>
-                <span className="text-small text-center">{ item.name }</span>
+        return (<div ref={ viewRef }>
+            <div ref={ ref } className={ styles.itemContainer + " d-flex flex-column justify-content-center align-items-center" + (focused ? " " + styles.itemfocused : "") } onClick={ () => onSelected() }>
+                <img src={ renderImage } alt="logo" className={ styles.itemCoverLogo }/>
+                <div className={ styles.itemCoverTitle }>
+                    <span className="text-small text-center">{ item.name }</span>
+                </div>
             </div>
-        </div>
+        </div>);
     }
 };
 
