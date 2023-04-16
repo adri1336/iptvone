720b8cf9e9bb477b74bdfdb975a412f0318ab09a
adri1samp <adri1pawn@gmail.com>
2023-04-16 12:37:42 +0200

    covers

--- components/item.jsx
+++ components/item.jsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/item.module.css";
 import IPTV from "@/utils/iptv";
 import { useFocusable } from "@noriginmedia/norigin-spatial-navigation";
+import { useState } from "react";
 
 const Item = ({ item, onSelected }) => {
     const { ref, focused } = useFocusable({
@@ -9,11 +10,38 @@ const Item = ({ item, onSelected }) => {
     });
 
     const raw = item.raw;
-
-    return <div ref={ ref } className={ styles.itemContainer + " d-flex flex-column justify-content-between align-items-center" + (focused ? " " + styles.itemfocused : "") } onClick={ () => onSelected() }>
-        <img src={ IPTV.getRawValue(raw, "tvg-logo") } alt="logo" className={ styles.itemLogo }/>
-        <span className="text-small text-center">{ item.name }</span>
-    </div>
+    const image = IPTV.getRawValue(raw, "tvg-logo");
+    
+    const [ aspectRatio, setAspectRatio ] = useState(1);
+    const img = new Image();
+    img.src = image;
+    img.onload = () => {
+        setAspectRatio(img.width / img.height);
+    };
+    img.onerror = () => {
+        setAspectRatio(0);
+    };
+    
+    const isCover = aspectRatio < 0.7;
+    
+    if(aspectRatio === 0) {
+        return <div ref={ ref } className={ styles.itemContainer + " d-flex flex-column justify-content-center align-items-center" + (focused ? " " + styles.itemfocused : "") } onClick={ () => onSelected() }>
+            <span className="text-medium text-center">{ item.name }</span>
+        </div>
+    }
+    else if(isCover) {
+        return <div ref={ ref } className={ styles.itemContainer + " d-flex flex-column justify-content-center align-items-center" + (focused ? " " + styles.itemfocused : "") } onClick={ () => onSelected() }>
+            <img src={ image } alt="logo" className={ styles.itemCoverLogo }/>
+            <div className={ styles.itemCoverTitle }>
+                <span className="text-small text-center">{ item.name }</span>
+            </div>
+        </div>
+    }
+    else 
+        return <div ref={ ref } className={ styles.itemContainer + " d-flex flex-column justify-content-center align-items-center" + (focused ? " " + styles.itemfocused : "") } onClick={ () => onSelected() }>
+            <img src={ image } alt="logo" className={ styles.itemLogo }/>
+            <span className="text-medium text-center">{ item.name }</span>
+        </div>
 };
 
 export default Item;
