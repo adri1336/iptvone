6b7285cad8c7700237cb7ae3ea84725e073b4eb6
adri1336 <adri1pawn@gmail.com>
2023-04-17 20:39:39 +0200

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -52,7 +52,8 @@ export default () => {
 				headers: {
 					'Accept': '*/*',
 					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
-				}
+				},
+				mode: 'cors'
 			});
 			if(res.ok) {
 				try {
